b458bee02726bbc3686f345c7a1f271f77b8fcc7
Airin_akter_nila <airinakhternila@gmail.com>
2026-05-30 21:12:38 +0600

    updated

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,7 @@ import jwt, { JwtPayload } from "jsonwebtoken"
 import config from '../config';
 
 // higher order function always func return kore
-// for git stick
+// for git
 
 const auth = () => {
     return (req: Request, res: Response, next: NextFunction) => {
